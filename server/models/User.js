5dcdeddbf6c72b97fa118ea12a15faf152f5eaa6
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-14 22:18:24 +0530

    commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+//schema mongoose
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
