8328f510d83dde5fac2267b71ce2f7e7fda4636d
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-16 22:58:54 +0530

    commit

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,6 +12,7 @@ export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   
   try {
+    //user exists
     const userExists = await User.findOne({ email });
     if (userExists) return res.status(400).json({ message: "User already exists" });
 
