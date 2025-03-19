a67e00554e5d2301fbca5a73b15d437b86c79f99
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-20 00:00:53 +0530

    commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 const Home = () => {
   return (
+    //welcome to skillconnect
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">Welcome to SkillConnect</h1>
       <p className="mt-4 text-lg text-gray-700">Connecting Mentors & Learners</p>
