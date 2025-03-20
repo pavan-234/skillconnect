8d88e733b921625025527c61c01955947a66d8ec
pavan-234 <varanasipavankalyan07@gmail.com>
2025-03-20 23:11:50 +0530

    user.js file

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,19 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isMentor: { type: Boolean, default: false },
+});
 
-const User = mongoose.model("User", userSchema);
-export default User;
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
